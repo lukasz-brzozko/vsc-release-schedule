0cf1ba2f16a1d877976af59ef8a08186304a0305
Łukasz Brzózko <lukasz9099@gmail.com>
2024-07-15 18:27:28 +0200

    feat: add basic script

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,164 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import moment from "moment";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const myTreeDataProvider = new MyTreeDataProvider();
+  vscode.window.registerTreeDataProvider("myTreeView", myTreeDataProvider);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "release-schedule-vsc" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "extension.showTreeView",
+    () => {
+      vscode.window.showInformationMessage(
+        "Tree View is now visible in the Explorer view."
+      );
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('release-schedule-vsc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from .!');
-	});
+  context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
+  vscode.commands.registerCommand("myTreeView.moveUp", (item: TreeItem) => {
+    const index = myTreeDataProvider.items.indexOf(item);
+    if (index > 0) {
+      myTreeDataProvider.moveItem(index, index - 1);
+      updateDates();
+    }
+  });
+
+  vscode.commands.registerCommand("myTreeView.moveDown", (item: TreeItem) => {
+    const index = myTreeDataProvider.items.indexOf(item);
+    if (index < myTreeDataProvider.items.length - 1) {
+      myTreeDataProvider.moveItem(index, index + 1);
+      updateDates();
+    }
+  });
+
+  vscode.commands.registerCommand(
+    "myTreeView.toggleChecked",
+    (item: TreeItem) => {
+      myTreeDataProvider.toggleItemChecked(item);
+      updateDates();
+    }
+  );
+
+  vscode.commands.registerCommand("extension.generateJSON", () => {
+    const data = myTreeDataProvider.items
+      .filter((item) => item.checked)
+      .map((item) => ({
+        label: item.label,
+        checked: item.checked,
+        nextMonday: item.nextMonday
+          ? item.nextMonday.format("DD-MM-YYYY")
+          : null,
+      }));
+
+    const jsonContent = JSON.stringify(data, null, 2);
+    vscode.workspace.fs.writeFile(
+      vscode.Uri.file("selectedItems.json"),
+      Buffer.from(jsonContent, "utf-8")
+    );
+
+    vscode.window.showInformationMessage("JSON file generated successfully.");
+  });
+
+  function updateDates() {
+    let startDate = moment().startOf("isoWeek").add(1, "week"); // Start from next Monday
+    let nextValidMonday = startDate.clone(); // Initialize the next valid Monday
+
+    myTreeDataProvider.items.forEach((item) => {
+      if (item.checked) {
+        item.nextMonday = nextValidMonday.clone();
+        item.updateNextMonday();
+        nextValidMonday.add(1, "week"); // Move to the next Monday for the next item
+      } else {
+        item.nextMonday = null;
+        item.updateNextMonday();
+      }
+    });
+  }
 }
 
-// This method is called when your extension is deactivated
+class TreeItem extends vscode.TreeItem {
+  public nextMonday: moment.Moment | null = null;
+
+  constructor(
+    public readonly originalLabel: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly index: number,
+    public checked: boolean = true,
+    public readonly command?: vscode.Command
+  ) {
+    super(originalLabel, collapsibleState);
+    this.contextValue = "treeItem";
+    this.updateNextMonday();
+  }
+
+  toggleChecked() {
+    this.checked = !this.checked;
+    this.updateNextMonday();
+  }
+
+  updateNextMonday() {
+    if (this.checked && this.nextMonday === null) {
+      let startDate = moment().startOf("isoWeek").add(1, "week"); // Start from next Monday
+      this.nextMonday = startDate.clone().add(this.index, "week");
+    } else if (!this.checked) {
+      this.nextMonday = null;
+    }
+
+    this.tooltip = this.nextMonday
+      ? `Next Monday: ${this.nextMonday.format("DD-MM-YYYY")}`
+      : "Not checked";
+    this.iconPath = this.checked
+      ? new vscode.ThemeIcon("check")
+      : new vscode.ThemeIcon("circle-outline");
+
+    if (this.checked && this.nextMonday) {
+      this.label = `${this.originalLabel} (${this.nextMonday.format(
+        "DD-MM-YYYY"
+      )})`;
+    } else {
+      this.label = this.originalLabel;
+    }
+  }
+}
+
+class MyTreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    TreeItem | undefined | null | void
+  > = new vscode.EventEmitter<TreeItem | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    TreeItem | undefined | null | void
+  > = this._onDidChangeTreeData.event;
+
+  public items: TreeItem[] = [
+    new TreeItem("Item 1", vscode.TreeItemCollapsibleState.None, 0),
+    new TreeItem("Item 2", vscode.TreeItemCollapsibleState.None, 1),
+    new TreeItem("Item 3", vscode.TreeItemCollapsibleState.None, 2),
+    new TreeItem("Item 4", vscode.TreeItemCollapsibleState.None, 3),
+  ];
+
+  getTreeItem(element: TreeItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: TreeItem): Thenable<TreeItem[]> {
+    if (element) {
+      return Promise.resolve([]);
+    } else {
+      return Promise.resolve(this.items);
+    }
+  }
+
+  moveItem(fromIndex: number, toIndex: number) {
+    const item = this.items.splice(fromIndex, 1)[0];
+    this.items.splice(toIndex, 0, item);
+    this._onDidChangeTreeData.fire();
+  }
+
+  toggleItemChecked(item: TreeItem) {
+    item.toggleChecked();
+    this._onDidChangeTreeData.fire(item);
+  }
+}
+
 export function deactivate() {}
